96679fe2263d216efcbecbbb7d3df2cf3ee83209
Kamil Fijalkowski <xenonenias@gmail.com>
2023-05-30 20:54:09 +0200

    Mini Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import "package:learning/gradient_container.dart";
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer.content(),
       ),
